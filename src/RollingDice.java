025f89ab5d8e3e3aebbc16d12ea9f64061058080
Christine Nielsen <christine.nielsen93@gmail.com>
2019-10-02 16:44:36 +0200

    tilføjet straf for to 3'ere

--- src/RollingDice.java
+++ src/RollingDice.java
@@ -65,7 +65,12 @@ public class RollingDice {
                     toEns1 = true;
                 }else if (terningTal3 == 1 && terningTal4 == 1){
                     toEns2 = true;
+                } else if (terningTal1 == 3 && terningTal2 == 3){
+                    toEns1 = true;
+                } else if (terningTal3 == 3 && terningTal4 == 3) {
+                    toEns2 = true;
                 } else
+
 
                     // Hvis ikke det er to 1'ere, lægger programmet den gamle sum til den nye, som er p1Sum og p2Sum
                     p1Sum = p1Sum + sum1;
