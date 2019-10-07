c8b6b2e1e2d272d0f3e0858b1e1d88ddcf0765a5
Christian Pone <ChristiaNotarmaso@gmail.com>
2019-10-07 16:04:30 +0200

    Tilføjet default kommando til test

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int face1 = 0, face2 = 0, face3 = 0, face4 = 0, face5 = 0, face6 = 0;
+        int face1 = 0, face2 = 0, face3 = 0, face4 = 0, face5 = 0, face6 = 0, face7 = 0;
         for (int i = 0; i < 1000; i++) {
             Die d = new Die();
             d.roll();
@@ -12,6 +12,9 @@ public class Test {
                 case 4: face4++; break;
                 case 5: face5++; break;
                 case 6: face6++; break;
+                case 7: face7++; break;
+                default:
+                    System.out.println("Der er en fejl i programmet"); break;
             }
         }
         System.out.println("Resultat af test:");
@@ -21,5 +24,6 @@ public class Test {
         System.out.println("Slog 4 " + face4 + " gange");
         System.out.println("Slog 5 " + face5 + " gange");
         System.out.println("Slog 6 " + face6 + " gange");
+        System.out.println("Slog 7 " + face7 + " gange");
     }
 }
